59601037e569580f561f24bb20dff17d5c1f2f86
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-11-06 11:34:53 -0500

    nuevo contrato

--- src/cons.js
+++ src/cons.js
@@ -1,4 +1,4 @@
 const proxy = "https://proxy-ewozx.herokuapp.com/";
 
-const SC = "TJcELfBcR2WEWUoj7acdHqBddGzaYhBVwW";// contrato tipo forsague USDT
+const SC = "TDL7ZoYdhNa5DZzyHkggSQ4wv5PvJXdkCA";// contrato tipo forsague USDT
 export default {proxy, SC};
