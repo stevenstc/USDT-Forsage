9d94fa5a2d9eed7eb73b08825d3d11a17bb6bd6e
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-11-16 14:38:27 -0500

    Update cons.js

--- src/cons.js
+++ src/cons.js
@@ -1,4 +1,6 @@
 const proxy = "https://proxy-ewozx.herokuapp.com/";
 
-const SC = "TDL7ZoYdhNa5DZzyHkggSQ4wv5PvJXdkCA";// contrato tipo forsague USDT
+//const SC = "TDL7ZoYdhNa5DZzyHkggSQ4wv5PvJXdkCA";// contrato tipo forsague USDT MainNet
+
+const SC = "TJcELfBcR2WEWUoj7acdHqBddGzaYhBVwW";// testnet TJcELfBcR2WEWUoj7acdHqBddGzaYhBVwW
 export default {proxy, SC};
